975345ca196bf67e573aabea2631e2e00d6d9d5e
xuuwon <dumpling1011@gmail.com>
2025-11-21 04:08:20 +0900

    feat: navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { FileText, HouseIcon, MapPin, UserRound } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+const Navigation = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex justify-around pt-3 h-[84px] border-t border-gray-g2 text-gray-g5">
+      <div
+        className={cn(
+          "flex flex-col items-center cursor-pointer gap-0.5",
+          pathname === "/" ? "text-main" : ""
+        )}
+      >
+        <HouseIcon />
+        <p className="text-xs font-pretendard">홈</p>
+      </div>
+
+      <div
+        className={cn(
+          "flex flex-col items-center cursor-pointer gap-0.5",
+          pathname === "/store" ? "text-main" : ""
+        )}
+      >
+        <MapPin />
+        <p className="text-xs font-pretendard">매장</p>
+      </div>
+
+      <div
+        className={cn(
+          "flex flex-col items-center cursor-pointer gap-0.5",
+          pathname === "/orders" ? "text-main" : ""
+        )}
+      >
+        <FileText />
+        <p className="text-xs font-pretendard">주문 내역</p>
+      </div>
+
+      <div
+        className={cn(
+          "flex flex-col items-center cursor-pointer gap-0.5",
+          pathname === "/profile" ? "text-main" : ""
+        )}
+      >
+        <UserRound />
+        <p className="text-xs font-pretendard">내 정보</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
