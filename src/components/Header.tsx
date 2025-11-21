79d02556aba94971a2558e8f723f4e4dad7f8d08
xuuwon <dumpling1011@gmail.com>
2025-11-21 23:21:11 +0900

    feat: 조건에 따른 UI 분리

---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { ChevronLeft, ShoppingCart } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+const Header = ({
+  title,
+  itemCount = 2,
+}: {
+  title: string;
+  itemCount?: number;
+}) => {
+  const pathname = usePathname();
+
+  return (
+    <div className="relative flex items-center h-[58px] mt-4">
+      <ChevronLeft
+        className={cn(
+          "absolute left-0 cursor-pointer",
+          pathname.startsWith("/map/") ? "" : "invisible"
+        )}
+      />
+
+      <h1 className="absolute left-1/2 -translate-x-1/2 text-[#26282B] text-lg font-bold">
+        {title}
+      </h1>
+
+      <div className="relative ml-auto">
+        <ShoppingCart size={22} className="cursor-pointer" />
+
+        {itemCount !== undefined && itemCount > 0 && (
+          <span
+            className="
+              absolute 
+              -bottom-1 
+              -right-1 
+              bg-[#00593A] 
+              text-white 
+              text-[10px] 
+              w-4 h-4 
+              flex items-center justify-center 
+              rounded-full
+            "
+          >
+            {itemCount}
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
