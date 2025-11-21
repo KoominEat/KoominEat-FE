e797cbaaf66700c4bf269512a887d32bd70645be
xuuwon <dumpling1011@gmail.com>
2025-11-22 05:27:54 +0900

    feat: 링크 이동 추가

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,6 +3,7 @@
 import { cn } from "@/lib/utils";
 import { FileText, HouseIcon, MapPin, UserRound } from "lucide-react";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 const Navigation = () => {
   const pathname = usePathname();
@@ -17,45 +18,66 @@ const Navigation = () => {
 
   return (
     <div className="flex justify-around pt-3 h-[84px] border-t border-gray-g2 text-gray-g5">
-      <div
+      {/* 홈 */}
+      <Link
+        href={pathname.startsWith("/courier") ? "/courier" : "/"}
         className={cn(
           "flex flex-col items-center cursor-pointer gap-0.5",
-          pathname === "/" ? "text-main" : ""
+          pathname === "/" || pathname === "/courier" ? "text-main" : ""
         )}
       >
         <HouseIcon />
         <p className="text-xs font-pretendard">홈</p>
-      </div>
+      </Link>
 
-      <div
+      {/* 매장 (courier 모드일 때 숨김) */}
+      {!pathname.startsWith("/courier") && (
+        <Link
+          href="/map"
+          className={cn(
+            "flex flex-col items-center cursor-pointer gap-0.5",
+            pathname === "/map" ? "text-main" : ""
+          )}
+        >
+          <MapPin />
+          <p className="text-xs font-pretendard">매장</p>
+        </Link>
+      )}
+
+      {/* 주문 내역 / 전달 내역 */}
+      <Link
+        href={
+          pathname.startsWith("/courier")
+            ? "/courier/delivery-history"
+            : "/order-history"
+        }
         className={cn(
           "flex flex-col items-center cursor-pointer gap-0.5",
-          pathname === "/map" ? "text-main" : ""
-        )}
-      >
-        <MapPin />
-        <p className="text-xs font-pretendard">매장</p>
-      </div>
-
-      <div
-        className={cn(
-          "flex flex-col items-center cursor-pointer gap-0.5",
-          pathname === "/order-history" ? "text-main" : ""
+          pathname === "/order-history" ||
+            pathname === "/courier/delivery-history"
+            ? "text-main"
+            : ""
         )}
       >
         <FileText />
-        <p className="text-xs font-pretendard">주문 내역</p>
-      </div>
+        <p className="text-xs font-pretendard">
+          {pathname.startsWith("/courier") ? "전달 내역" : "주문 내역"}
+        </p>
+      </Link>
 
-      <div
+      {/* 내정보 */}
+      <Link
+        href={pathname.startsWith("/courier") ? "/courier/profile" : "/profile"}
         className={cn(
           "flex flex-col items-center cursor-pointer gap-0.5",
-          pathname === "/profile" ? "text-main" : ""
+          pathname === "/profile" || pathname === "/courier/profile"
+            ? "text-main"
+            : ""
         )}
       >
         <UserRound />
         <p className="text-xs font-pretendard">내 정보</p>
-      </div>
+      </Link>
     </div>
   );
 };
