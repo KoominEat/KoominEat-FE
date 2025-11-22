56d0e5e891ffcf99fe86cad6a23acde31fbc113c
xuuwon <dumpling1011@gmail.com>
2025-11-22 11:20:45 +0900

    fix: map component suspense 추가로 배포 오류 해결

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import Map from "./components/Map";
 
 const page = () => {
-  return <Map />;
+  return (
+    <Suspense fallback={<div>로딩 중...</div>}>
+      <Map />
+    </Suspense>
+  );
 };
 
 export default page;
